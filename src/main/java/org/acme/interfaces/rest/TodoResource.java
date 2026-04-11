2e1c482343bbb22c371abfa49ec400f4c8e53abf
JEdaurdoPA <A01666860@tec.mx>
2026-04-10 19:41:45 -0600

    feat: agregar funcionalidad de eliminar todo

--- src/main/java/org/acme/interfaces/rest/TodoResource.java
+++ src/main/java/org/acme/interfaces/rest/TodoResource.java
@@ -1,7 +1,9 @@
 package org.acme.interfaces.rest;
 
 import jakarta.inject.Inject;
+import jakarta.transaction.Transactional;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
@@ -27,5 +29,18 @@ public class TodoResource {
     public Response createTodo(CreateTodoDto todoDto) {
         Todo todo= createTodoUseCase.execute(todoDto);
         return Response.ok(todo).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id) {
+
+        boolean deleted = repo.deleteById(1L);
+
+        return deleted
+        ? Response.noContent().build()
+        : Response.status(404).build();
+
     }
 }
